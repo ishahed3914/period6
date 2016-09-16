196a69f684f58f3792e2288fc6a11b15b05ebd27
ishahed3914 <ishahed3914@users.noreply.github.com>
2016-09-16 11:53:10 -0400

    Update ProceduralExample.java

--- IramIntroduction/src/introUnit/ProceduralExample.java
+++ IramIntroduction/src/introUnit/ProceduralExample.java
@@ -11,6 +11,6 @@ public class ProceduralExample {
 	public static void main (String[] args) {
 		String[] names = {"Jillian","Joseph","Jordan"};
 		String[]years = {"Senior","Junior","Sophmore"};
-		System.out.println("Hello my name is"+ names[0]+"and I am a" );
+		System.out.println("Hello my name is"+ names[0]+"and I am a"+ years[0] );
 	}
 }
