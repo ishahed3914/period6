4f3fe6aaaac69e88d19f3daa4a1d5f0c0c78c53f
Student 8 <Student 8@10.8.33.155>
2016-09-20 12:24:16 -0400

    yutybh

--- IramIntroduction/src/dataTypes/DataTypes.java
+++ IramIntroduction/src/dataTypes/DataTypes.java
@@ -26,6 +26,12 @@ public class DataTypes {
 		System.out.println(5/2);
 		System.out.println(3+5.0/2+5*2);
 		System.out.println((int)(3.0 + 5)/(2 + 5 * 2));
+		String ten = "10";
+		String two = "2";
+		if(ten.compareTo(two)>0) System.out.println("10 is greater than two");
+		else System.out.print("10 is not greater than two");
+		
+		
 	}
 
 }
