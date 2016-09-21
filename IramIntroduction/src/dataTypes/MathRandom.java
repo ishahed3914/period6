18f6328ec35ed683f86fc2bc3f81b8dd69223c21
Student 8 <Student 8@10.8.33.155>
2016-09-21 13:17:54 -0400

    dyo

--- IramIntroduction/src/dataTypes/MathRandom.java
+++ IramIntroduction/src/dataTypes/MathRandom.java
@@ -1,10 +1,35 @@
 package dataTypes;
 
+import java.util.Random;
+
 public class MathRandom {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int[] results = new int[6];
+		//same as
+		//int [] results = {0,0,0,0,0,0};
+		int totalRolls = 10000;
+		for(int index = 0; index < totalRolls; index++){
+			int roll = rollFairDie();
+			System.out.println("Roll #" + (index+1)+ " Die is " + rollFairDie());
+			results[roll-1]++;
+		}
+		for(int i = 0; i < 6; i++)
+		System.out.println((i + 1) + " was rolled " +(int)((double)results[i]/totalRolls *1000)/10.0 +
+				"% of the time");
+	}
+	public static int rollFairDie(){
+//		Random gen = new Random();
+//		return gen.nextInt(6)+1;
+		double rand = Math.random();//random double (0.1)
+		int roll = (int) (6 * rand);// [0,5](this is what above code does)
+		return roll + 1;//0 becomes 1, 5 becomes 6
+		}
+	public static int rollUnfairDie(){
+		double rand = Math.random();//random double (0.1)
+		int roll = (int) (6 * rand);// [0,5](this is what above code does)
+		return roll + 1;
 	}
 
 }
---
+++ IramIntroduction/src/dataTypes/StringPractice.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package dataTypes;
+
+/**
+ * @author Student 8
+ *
+ */
+public class StringPractice {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//String text = new String("Hello World");
+		String text1 = "Hello World";//same as above
+		String text2 = "Hello";//same as above
+		String text3 = " World";
+		if(text1.equals(text2+text3)){
+			System.out.println("These strings are equal.");
+		}
+		System.out.println(text1);
+		System.out.println(text2+ text3);
+		
+		String word1 = "Aardvark";
+		String word2 = "Zyzzyva";
+		if(word1.compareTo(word2) < 0){
+			System.out.println("Word1 is before word 2." + "lexicongraphically.");
+		}
+	}
+
+}
