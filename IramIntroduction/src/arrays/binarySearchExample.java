36dcc5136f5bf52771c35dd956c15810b54ce6ab
Student 8 <Student 8@10.8.33.155>
2016-10-21 13:18:54 -0400

    dasdfas

--- IramIntroduction/src/arrays/binarySearchExample.java
+++ IramIntroduction/src/arrays/binarySearchExample.java
@@ -4,6 +4,8 @@ public class binarySearchExample {
 
 	public static void main(String[] args) {
 		int [] arr = {2,3,4,6,9,11,12,15};
+		swap(arr, 0, arr.length-1);
+		shuffle(arr);
 		if(checkHalfway(arr,12,0, arr.length-1)){
 			System.out.println("The number you are searching for is less than the value in the"
 					+ "middle of the array");
@@ -13,6 +15,23 @@ public class binarySearchExample {
 					+ "in the middle of the array");
 		}
 	
+	}
+	private static void shuffle(int[] arr) {
+		for(int i = 0; i < arr.length; i++){
+			int random = (int)(Math.random() *arr.length);
+			swap(arr,i,random);
+		}
+	}
+	private static void print(int[] arr){
+		for(int i =0; i <arr.length-1; i++){
+			System.out.println(arr[i]+",");
+
+		}
+	}
+	private static void swap(int[] arr, int i, int j) {
+		int placeholder = arr[j];
+		arr[j] = arr[i];
+		arr[i] = placeholder;
 	}
 	/**
 	 * returns true if SearchValue is less than element
