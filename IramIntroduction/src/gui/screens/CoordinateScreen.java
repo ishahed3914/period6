0a78c421e6f49c0c8124e5030355193f090ab6d2
Student 8 <Student 8@10.8.33.155>
2016-12-14 13:16:38 -0500

    sadasfasfasfasfas

---
+++ IramIntroduction/src/gui/screens/CoordinateScreen.java
@@ -0,0 +1,27 @@
+package gui.screens;
+
+import java.awt.Color;
+import java.util.ArrayList;
+
+import gui.Screen;
+import gui.components.Action;
+import gui.components.Button;
+import gui.components.TextLabel;
+import gui.components.Visible;
+
+public class CoordinateScreen extends Screen {
+//	private Button myButton = new Button(40,50,100,30,"Button",new Color(0,76,153), new Action());
+	public CoordinateScreen(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initObjects(ArrayList<Visible> viewObjects) {
+		TextLabel text = new TextLabel(20, 200, 500, 40, "Some Text~");
+		viewObjects.add(text);
+//		viewObjects.add(myButton);
+		
+	}
+
+}
